6ddc2de503b155b79a84b28ba5b2ca2de3478350
Alex <alex.creativecode@gmail.com>
2021-04-07 08:45:53 +0300

    adadadad

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,94 @@
 const path = require('path')
-const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = function (env, argv) {
-  return {
-    entry: ['@babel/polyfill', './src/index.js'],
-    output: {
-      path: path.join(__dirname, './build/'),
-      filename: './js/script.js',
-      publicPath: '/'
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.join(__dirname, './build/'),
+    filename: './js/script.js',
+    publicPath: '/'
+  },
+  devServer: {
+    index: 'index.html',
+    contentBase: path.resolve(__dirname, './build/'),
+    publicPath: '/',
+    port: 8000,
+    host: 'localhost',
+    historyApiFallback: {
+      index: 'index.html'
     },
-    module: {
-      rules: [
-        {
-          test: /\.js?$/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-                presets: ['@babel/preset-env']
-              }
-            },
-            'eslint-loader'
-          ],
-          exclude: /node_modules/
-        },
-        {
-          test: /\.scss$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-        },
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-        },
-        {
-          test: /\.js$/,
-          use: ['source-map-loader'],
-          enforce: 'pre'
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: './css/style.css'
+    open: true,
+    watchContentBase: true,
+    disableHostCheck: true,
+    setup(app) {
+      app.post('*', (req, res) => {
+        res.redirect(req.originalUrl)
       })
-    ],
-    optimization: {
-      minimizer: [
-        new OptimizeCSSAssetsPlugin({
-          cssProcessorOptions: {
-            safe: true,
-            discardComments: {
-              removeAll: true
-            }
-          }
-        }),
-        new TerserPlugin({
-          parallel: true,
-          terserOptions: {
-            ecma: 5,
-            safari10: true,
-            ie8: true,
-            output: {
-              comments: false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['@babel/preset-env']
             }
           },
-          extractComments: false
-        })
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    }
+          'eslint-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre'
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/style.css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          safe: true,
+          discardComments: {
+            removeAll: true
+          }
+        }
+      }),
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 5,
+          safari10: true,
+          ie8: true,
+          output: {
+            comments: false
+          }
+        },
+        extractComments: false
+      })
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 }
